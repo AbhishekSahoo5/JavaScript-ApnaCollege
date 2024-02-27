4837404cc51b4ff6b2778394adc0acafdb0eb68b
Abhishek Sahoo <abhisheksahoo553@gmail.com>
2024-02-27 22:04:35 +0530

    DOM

---
+++ 29_window.js
@@ -0,0 +1,8 @@
+/*
+Window Object
+-------------> The window object represents an open window in a browser. It is browser's object (not javascript's) and is automatically created by browser.
+
+It is a global object with lots of properties & methods
+*/
+// Window.console.log("Abhishek");
+console.log("Abhishek");
